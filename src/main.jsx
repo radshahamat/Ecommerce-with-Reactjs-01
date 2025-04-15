1484a7c083927d52de4e8552f2f63739b7dc7d4d
radshahamat <radshamat@gmail.com>
2025-04-15 16:49:32 +0600

    Latest collection section added

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
-import React from 'react'; // Add this line
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import ShopContextProvider from './context/ShopContext'
 import './index.css'
-import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
-import ShopContextProvider from './context/ShopContext.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <ShopContextProvider >
-      <App />
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ShopContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ShopContextProvider>
-  </BrowserRouter>,
+  </React.StrictMode>
 )
-  
